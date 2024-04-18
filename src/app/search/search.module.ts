266dffc60830692eb834a89f86958b3f9f848c68
Cassiano-lamarc96 <cacassiano2011@hotmail.com>
2024-04-18 01:02:55 -0300

    search input css initial

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchIndexComponent } from './search-index/search-index.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { SearchIndexComponent } from './search-index/search-index.component';
   ],
   imports: [
     CommonModule,
-    SearchRoutingModule
+    SearchRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class SearchModule { }
